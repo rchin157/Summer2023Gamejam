b88200733d5328c8119653232ab81efeaa07ed3b
Xulxuvar <teridax1306@gmail.com>
2023-07-07 18:53:01 -0600

    More Base Files
    Yuh

---
+++ Scripts/Ball.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+var ballMesh
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ballMesh = $BallMesh;
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
