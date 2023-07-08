a3e9ef745681ea7f495ec62cfbe0704b391a68e5
Xulxuvar <teridax1306@gmail.com>
2023-07-07 23:25:44 -0600

    Player+ball tracaking
    yeh

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,13 +1,23 @@
-extends Node3D
+extends CharacterBody3D
 
-var ballMesh
+@onready var navAgent = $NavigationAgent3D
+
+const SPEED = 3.0;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ballMesh = $BallMesh;
 	pass # Replace with function body.
 
+func updateTargetLocation(targetLocation):
+	navAgent.target_position = targetLocation
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var current_location = global_transform.origin
+	var next_location = navAgent.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * SPEED
 	
+	velocity = new_velocity
+	move_and_slide()
+	rotate_z( -1.0*delta*velocity.x)
+	rotate_x( 1.0*delta*velocity.z)
 	pass
